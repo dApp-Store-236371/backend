39773382e0c7674cf7a5db00d871236c99a53190
Eliran <eliran9692@gmail.com>
2022-03-26 22:09:17 +0300

    fetch apps with criteria

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from email.policy import HTTP
 from turtle import down
 from fastapi import FastAPI, status
-from models.app_model import App, AppCollection
+from models.app_model import App, AppCollection, AppRequestBody
 from fastapi.responses import JSONResponse
 
 api_app = FastAPI()
 
 sandbox_db = AppCollection({
-    "A": App("A", "d1"),
-    "B": App("B", "d2")
+    "A": App(1, "A", "d1", 10, "img1", "sha1", "creator1"),
+    "B": App(2, "B", "d2", 20, "img2", "sha2", "creator2"),
+    "C": App(3, "C", "d3", 30, "img3", "sha3", "creator1"),
+    "D": App(4, "D", "d4", 20, "img2", "sha2", "creator3"),
+    "E": App(5, "E", "d5", 20, "img2", "sha2", "creator4")
     })
 
 
@@ -20,7 +23,7 @@ def root():
 @api_app.get("/download/{app_name}")
 async def fetch_download_link(app_name: str) -> str:
     app_obj = sandbox_db[app_name]
-    
+
     if not app_obj:
         return JSONResponse(content={"link": None},
                             status_code=status.HTTP_404_NOT_FOUND)
@@ -32,3 +35,28 @@ async def fetch_download_link(app_name: str) -> str:
 
     return JSONResponse(content={"link": download_link},
                         status_code=status.HTTP_200_OK)
+
+
+@api_app.post("/apps/")
+async def fetch_apps(req: AppRequestBody):
+    contains = req.filters.get("name", "")
+
+    start = req.start
+    end = req.end
+    use_bounds = start != -1 and end != -1
+
+    creator = req.id
+    response = {}
+
+    for idx, app in enumerate(sandbox_db):
+        if app.creator == creator and contains in app.name:
+            if use_bounds:
+                if start <= app.id <= end:
+                    response[idx] = app.jsonify()
+            else:
+                response[idx] = app.jsonify()
+
+    return JSONResponse(content=response,
+                        status_code=status.HTTP_200_OK)
+
+
--- models/app_model.py
+++ models/app_model.py
@@ -1,12 +1,35 @@
-class App():
+from typing import Optional, Union
+from datetime import date
+
+from pydantic import BaseModel
+
+
+class App:
+    id: int
     _name: str
     _download_link: str
-    ...
-    # category, price
+    price: int
+    rating: float
+    description: str
+    img_url: str
+    sha: str
+    version: int
+    publication_date: str
+    creator: str
 
-    def __init__(self, name: str, download_link: str) -> None:
+    def __init__(self, id: int, name: str, download_link: str, price: int, img_url: str, sha: str,
+                 creator: str, desc: str = "", version: int = 1, rating: int = None) -> None:
+        self.id = id
         self._name = name
         self._download_link = download_link
+        self.price = price
+        self.rating = rating
+        self.description = desc
+        self.img_url = img_url
+        self.sha = sha
+        self.version = version
+        self.publication_date = str(date.today())
+        self.creator = creator
 
     @property
     def download_link(self):
@@ -18,6 +41,21 @@ class App():
 
     def __str__(self) -> str:
         return f"App {self._name} can be downloaded from {self._download_link}"
+
+    def jsonify(self) -> dict:
+        return {
+            "id": self.id,
+            "name": self._name,
+            "download_link": self._download_link,
+            "price": self.price,
+            "rating": self.rating,
+            "description": self.description,
+            "img_url": self.img_url,
+            "sha": self.sha,
+            "version": self.version,
+            "publication_date": self.publication_date,
+            "creator": self.creator
+        }
 
 
 class AppCollection:
@@ -36,6 +74,18 @@ class AppCollection:
     def __getitem__(self, key) -> App:
         return self.apps.get(key)
 
+    def __iter__(self) -> Union[str, App]:
+        for k, v in self.apps.items():
+            yield v
+
+
+
+class AppRequestBody(BaseModel):
+    id: str # creator id?
+    filters: dict
+    start: Optional[int] = -1
+    end: Optional[int] = -1
+    # order: dict
     
 
 
